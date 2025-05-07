30f25c9694f69908c367b08f86c1e2d290dca6d3
savom <56549021+savom@users.noreply.github.com>
2025-05-07 20:23:39 +0900

    250507

--- practice.py
+++ practice.py
@@ -1,4 +1,21 @@
-a={'apple','banana','kiwi'}
+key_list = ["name", "hp", "mp", "level"]
+value_list = ["기사", 200, 30, 5]
+character = {}
 
-for i in a:
-    print("과일명 : %s" % i)
+for i in range(0, len(key_list)):
+    character[key_list[i] value_list[i]]
+
+print(character)
+
+# 확인 문제3
+limit = 10000
+i= 1
+sum_value = 0
+
+while (sum_value < limit):
+    sum_value += i
+    i += 1
+    
+print("{}를 더할 때 {}을 넘으며 그때의 값은 {}입니다.".format(i-1,limit,sum_value))
+
+# 확인 문제4

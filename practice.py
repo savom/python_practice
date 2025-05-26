d8c42fe599af452abfe27800d54d876b18176c11
savom <56549021+savom@users.noreply.github.com>
2025-05-26 17:20:30 +0900

    250526 파일처리까지

--- practice.py
+++ practice.py
@@ -1,21 +1,6 @@
-key_list = ["name", "hp", "mp", "level"]
-value_list = ["기사", 200, 30, 5]
-character = {}
-
-for i in range(0, len(key_list)):
-    character[key_list[i] value_list[i]]
-
-print(character)
-
-# 확인 문제3
-limit = 10000
-i= 1
-sum_value = 0
-
-while (sum_value < limit):
-    sum_value += i
-    i += 1
+s= ["Seoul","Busan", "jeju", "Gwangju", "Incheon"]
+a = ""
+for i in s:
+    a+=i[2]
     
-print("{}를 더할 때 {}을 넘으며 그때의 값은 {}입니다.".format(i-1,limit,sum_value))
-
-# 확인 문제4
+print(a)
---
+++ test69.py
@@ -0,0 +1,6 @@
+[a,b] = [10,20]
+(c,d) = (10,20)
+tprint("a:",a)
+print("b:",b)
+print("c:",c)
+print("d:",d)
